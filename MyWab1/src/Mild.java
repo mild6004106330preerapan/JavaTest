746ac67763e16ece0919e9afa45cb249266f079f
Preerapan <mildlovemam503306@gmail.com>
2018-07-24 11:39:53 +0700

    version 1

---
+++ MyWab1/src/Mild.java
@@ -0,0 +1,26 @@
+
+public class Mild {
+	private int age;
+	private String name;
+	
+public Mild(){
+	
+}
+public void mild(int a,String n) {
+	age=a;
+	name=n;
+}
+public int getage() {
+	return age;
+}
+public void setage01(int a) {
+	age=a;
+}
+public String getname() {
+	return name;
+}
+public void setage02(String n) {
+	name=n;
+}
+
+}
